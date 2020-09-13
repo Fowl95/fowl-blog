4b6afa2d350eb05fd67c6b496a90dec7bf370285
Jacopo Michettoni <jacopofowl@gmail.com>
2020-09-13 17:40:33 +0200

    Image update

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -56,7 +56,7 @@ class BlogPostTemplate extends React.Component {
           </li>
           <li>
             {next && (
-              <Link to={`/blog${next.fields.slug}`} rel="next">
+              <Link to={`/${next.fields.slug}`} rel="next">
                 {next.frontmatter.title} →
               </Link>
             )}
