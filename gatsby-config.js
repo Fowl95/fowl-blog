71a81a658217d91be688da57b87c397fe06b77f6
Jacopo Michettoni <jacopofowl@gmail.com>
2020-09-13 17:01:11 +0200

    Update name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Fowl's Blog`,
+    author: `Fowl`,
+    description: `A Writing and Learning Blog.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `gatsbyjs`,
