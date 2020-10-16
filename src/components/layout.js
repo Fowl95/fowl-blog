eccd778e917aa01395658e0c4d60b6d956a09388
Jacopo Michettoni <jacopofowl@gmail.com>
2020-10-16 10:10:31 +0200

    rootpath

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    const blogPath = `${__PATH_PREFIX__}/`
+    const blogPath = `${__PATH_PREFIX__}/blog/`
     let header
 
     if (location.pathname === rootPath || location.pathname === blogPath) {
@@ -26,7 +26,7 @@ class Layout extends React.Component {
               textDecoration: `none`,
               color: `inherit`,
             }}
-            to={location.pathname === blogPath ? `/blog/` : `/`}
+            to={location.pathname === rootPath ? `/` : `/`}
           >
             {title}
           </Link>
